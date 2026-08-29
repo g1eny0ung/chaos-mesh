ea7305dbdcdfc597c8a5b2c301863df70fd6f8a9
Yue Yang <g1enyy0ung@gmail.com>
2026-08-29 19:33:30 +0800

    chore: use vite 8
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- ui/app/vite.config.ts
+++ ui/app/vite.config.ts
@@ -1,5 +1,5 @@
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import react from '@vitejs/plugin-react'
+import { URL, fileURLToPath } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 import svgr from 'vite-plugin-svgr'
 
@@ -11,11 +11,7 @@ export default defineConfig(({ mode }) => {
     base: './',
     resolve: {
       alias: {
-        // https://github.com/react-dnd/react-dnd/issues/3416
-        'react/jsx-runtime.js': 'react/jsx-runtime',
-        'react/jsx-dev-runtime.js': 'react/jsx-dev-runtime',
-
-        '@': path.resolve(__dirname, './src'),
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
     plugins: [react(), svgr()],
